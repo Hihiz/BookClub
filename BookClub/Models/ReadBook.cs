ac11bb50a28669b63a1f490c1bc005605d84c56e
Hihiz <public@mail.ru>
2023-06-19 19:00:03 +0300

    Модели

---
+++ BookClub/Models/ReadBook.cs
@@ -0,0 +1,9 @@
+﻿namespace BookClub.Models
+{
+    public class ReadBook
+    {
+        public int Id { get; set; }
+        public int BookId { get; set; }
+        public Book Book { get; set; }
+    }
+}
