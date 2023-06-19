e957ce22cc6d606666b51439397fbc5309961d9e
Hihiz <public@mail.ru>
2023-06-19 21:06:03 +0300

    Сделать методы действия для просмотра списка книг, добавления в список прочитанных, просмотр добавленных книг, удаление

--- BookClub/Controllers/HomeController.cs
+++ BookClub/Controllers/HomeController.cs
@@ -1,21 +1,71 @@
-﻿using BookClub.Models;
+﻿using BookClub.Data;
+using BookClub.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace BookClub.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ApplicationDbContext db) => _db = db;
+
+        public async Task<IActionResult> Index() => View(_db.Books.ToList());
+
+        public async Task<IActionResult> AddListBook(int id)
         {
-            _logger = logger;
+            if (id == 0)
+            {
+                return NotFound();
+            }
+
+            Book book = await _db.Books.FindAsync(id);
+
+            if (ModelState.IsValid)
+            {
+                ReadBook readBook = new ReadBook()
+                {
+                    BookId = book.Id,
+                    UserId = 1
+                };
+
+                try
+                {
+                    _db.ReadBooks.Add(readBook);
+                    _db.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    return NotFound();
+                }
+            }
+
+            return RedirectToAction(nameof(Index));
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> CheckList(int id = 1)
         {
-            return View();
+            var applicationContext = _db.ReadBooks.Include(r => r.Book).Include(r => r.User).Where(r => r.UserId == id);
+
+            return View(await applicationContext.ToListAsync());
+        }
+
+        public async Task<IActionResult> DeleteListBook(int id)
+        {
+            ReadBook readBook = await _db.ReadBooks.FindAsync(id);
+
+            if (readBook != null)
+            {
+                _db.ReadBooks.Remove(readBook);
+            }
+
+            await _db.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
         }
 
         public IActionResult Privacy()
